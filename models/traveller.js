a32cfdb02806fae09d93fe4ba7cbf8b9dcf08b3f
Ernest Fraiman <upapaus@gmail.com>
2018-11-21 16:40:20 +0000

    4 functions written

--- models/traveller.js
+++ models/traveller.js
@@ -3,19 +3,23 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const startLocations = this.journeys.map(journey => journey.startLocation);
+  return startLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const endLocations = this.journeys.map(journey => journey.endLocation);
+  return endLocations;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const filteredJourneys = this.journeys.filter(journey => journey.transport === transport);
+  return filteredJourneys;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const filteredJourneys = this.journeys.filter(journey => journey.distance >= minDistance);
+  return filteredJourneys;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
